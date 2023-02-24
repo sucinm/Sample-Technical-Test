86e008956f6bdd319aaf68133f20788c6715fe49
sucinm <suci.nascaisar@gmail.com>
2023-02-24 22:17:33 +0700

    minimize logic to get minimum bus required

--- src/MinimumBusRequired/Main.java
+++ src/MinimumBusRequired/Main.java
@@ -6,19 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] familySizes = new int[n];
         int passenger = 0;
-        int count = 0; int i = 0;
+        int count = 0;
         while (scanner.hasNextInt()) {
             int num = scanner.nextInt();
-            if(count < familySizes.length) {
-                familySizes[count] = num;
-                passenger += num;
-            }
+            passenger += num;
             count++;
         }
 
-        if(count > familySizes.length){
+        if(count != n){
             System.out.print("Input must be equal with count of family");
         } else {
             System.out.println("Minimum bus required is : " + (int) Math.ceil(passenger / 4.0));
